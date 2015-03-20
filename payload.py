116e0f435af0a70f2c7a0c2f08dff17a4d0a3e63
shunghsiyu <shunghsiyu@github.com>
2015-03-20 20:27:47 +0800

    Add payload.py
    Add a data class for storing payload

---
+++ payload.py
@@ -0,0 +1,17 @@
+__author__ = 'shunghsiyu'
+
+class Payload(object):
+    def __init__(self, H, D, M):
+        _H = H
+        _D = D
+        _M = M
+
+    def D(self):
+        return _D
+
+
+    def H(self):
+        return _H
+
+    def M(self):
+        return _M
