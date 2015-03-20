52ca8284bcf04384e8e7bc2a09f39c75e4c32bb0
shunghsiyu <shunghsiyu@github.com>
2015-03-20 20:28:35 +0800

    Update original.py
    Add code to partially implement enc() method

--- original.py
+++ original.py
@@ -1,3 +1,9 @@
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Util import Counter
+
 __author__ = 'shunghsiyu'
 
 class Original:
@@ -5,7 +11,17 @@ class Original:
         print('init')
 
     def enc(self, data, A, B):
-        print('enc')
+        n = 32
+        ctr = Counter.new(128, initial_value=1L)
+
+        k = Random.get_random_bytes(n)
+        aes = AES.new(k, AES.MODE_CTR, ctr)
+        D = aes.encrypt(data)
+        M = HMAC.new(k, SHA256).digest()
+        S = ''
+        H = ''
+        raise NotImplementedError()
+        return H + D + M
 
     def dec(self, payload):
         print('dec')
