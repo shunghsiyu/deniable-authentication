7c9e3fe48f31ade95327deb30d20375414715f1e
shunghsiyu <shunghsiyu@gmail.com>
2015-03-24 08:35:07 -0400

    Update payload.py
    Implement method enc() fully
    Rearrange and refactor code for readability
    Use the Container class for serializing A, B k, S together in method
    enc()

--- original.py
+++ original.py
@@ -1,12 +1,13 @@
 from Crypto import Random
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Cipher.PKCS1_OAEP import PKCS1OAEP_Cipher
 from Crypto.Hash import HMAC, SHA256
 from Crypto.PublicKey import RSA
 from Crypto.PublicKey.RSA import _RSAobj
 from Crypto.Signature import PKCS1_PSS
 from Crypto.Signature.PKCS1_PSS import PSS_SigScheme
 from Crypto.Util import Counter
-from payload import Payload
+from payload import Payload, Container
 
 __author__ = 'shunghsiyu'
 
@@ -15,26 +16,45 @@ class Original:
         print('init')
 
     def enc(self, data, A, B):
-        assert isinstance(A, _RSAobj)
-        assert isinstance(B, _RSAobj)
         n = 32
-        ctr = Counter.new(128, initial_value=1L)
+        iv = 1L
+        ctr = Counter.new(128, initial_value=iv)
+
+        priA = self.privatekey(A)
+        assert isinstance(priA, _RSAobj)
+        pubB = self.publickey(B)
+        assert isinstance(pubB, _RSAobj)
+        signer = PKCS1_PSS.new(priA)
+        assert isinstance(signer, PSS_SigScheme)
+        rsa = PKCS1_OAEP.new(pubB, SHA256)
+        assert isinstance(rsa, PKCS1OAEP_Cipher)
 
         k = Random.get_random_bytes(n)
-        aes = AES.new(k, AES.MODE_CTR, ctr)
-        hmac = HMAC.new(k, SHA256)
-        sha256 = SHA256.new()
-        signer = PKCS1_PSS.new(A)
-        D = aes.encrypt(data)
+
+        aesk = AES.new(k, AES.MODE_CTR, ctr)
+        D = aesk.encrypt(data)
+
+        hmac = HMAC.new(k, D, SHA256)
         M = hmac.digest()
-        assert isinstance(signer, PSS_SigScheme)
-        hashABk = SHA256.new(A + B + k).digest()
+
+        ABk = Container(A, B, k).serialize()
+        hashABk = SHA256.new(ABk).digest()
         S = signer.sign(hashABk)
-        H = ''
-        raise NotImplementedError()
-        return Payload(H, D, M)
+
+        ABkS = Container(A, B, k, S).serialize()
+        H = rsa.encrypt(ABkS)
+        return Payload(H, D, M).serialize()
 
     def dec(self, payload):
         print('dec')
+        raise NotImplementedError()
+
+    def publickey(self, target):
+        raise NotImplementedError()
+        return None
+
+    def privatekey(self, target):
+        raise NotImplementedError()
+        return None
 
 raise NotImplementedError()
