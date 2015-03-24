a9add41d3cae205b79885357c34c34d3ee00d3ba
shunghsiyu <shunghsiyu@gmail.com>
2015-03-24 07:20:53 -0400

    Update original.py
    Use Payload, PSS_SigScheme and PKCS1_PSS class

--- original.py
+++ original.py
@@ -2,7 +2,11 @@ from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.PublicKey import RSA
+from Crypto.PublicKey.RSA import _RSAobj
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Signature.PKCS1_PSS import PSS_SigScheme
 from Crypto.Util import Counter
+from payload import Payload
 
 __author__ = 'shunghsiyu'
 
@@ -11,17 +15,24 @@ class Original:
         print('init')
 
     def enc(self, data, A, B):
+        assert isinstance(A, _RSAobj)
+        assert isinstance(B, _RSAobj)
         n = 32
         ctr = Counter.new(128, initial_value=1L)
 
         k = Random.get_random_bytes(n)
         aes = AES.new(k, AES.MODE_CTR, ctr)
+        hmac = HMAC.new(k, SHA256)
+        sha256 = SHA256.new()
+        signer = PKCS1_PSS.new(A)
         D = aes.encrypt(data)
-        M = HMAC.new(k, SHA256).digest()
-        S = ''
+        M = hmac.digest()
+        assert isinstance(signer, PSS_SigScheme)
+        hashABk = SHA256.new(A + B + k).digest()
+        S = signer.sign(hashABk)
         H = ''
         raise NotImplementedError()
-        return H + D + M
+        return Payload(H, D, M)
 
     def dec(self, payload):
         print('dec')
