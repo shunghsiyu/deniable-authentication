bb57d2f27484841535414c1f26b0b6b364d65c15
shunghsiyu <shunghsiyu@gmail.com>
2015-03-24 08:24:32 -0400

    Update payload.py
    Add serialize method for Payload class
    Add a new data class Container

--- payload.py
+++ payload.py
@@ -1,3 +1,5 @@
+import pickle
+
 __author__ = 'shunghsiyu'
 
 class Payload(object):
@@ -15,3 +17,28 @@ class Payload(object):
 
     def M(self):
         return _M
+
+    def serialize(self):
+        return pickle.dump(self)
+
+class Container(object):
+    def __init__(self, A, B, k, S = None):
+        _A = A
+        _B = B
+        _k = k
+        _S = S
+
+    def A(self):
+        return _A
+
+    def B(self):
+        return _B
+
+    def k(self):
+        return _k
+
+    def S(self):
+        return _S
+
+    def serialize(self):
+        return pickle.dump(self)
