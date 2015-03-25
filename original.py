330a1c41156c2beb8eff07f9d21975919d47d748
shunghsiyu <shunghsiyu@gmail.com>
2015-03-25 09:43:37 -0400

    Update original.py
    Move constants declaration to constructor
    Change method signature to privatekey() to take advantage of object
    oriented programming

--- original.py
+++ original.py
@@ -12,15 +12,15 @@ from payload import Payload, Container
 __author__ = 'shunghsiyu'
 
 class Original:
-    def __init__(self):
-        print('init')
+    def __init__(self, identity):
+        self._identity = identity
+        self._n = 32
+        self._iv = 1L
+        self._ctr = Counter.new(128, initial_value=self._iv)
 
     def enc(self, data, A, B):
-        n = 32
-        iv = 1L
-        ctr = Counter.new(128, initial_value=iv)
 
-        priA = self.privatekey(A)
+        priA = self.privatekey()
         assert isinstance(priA, _RSAobj)
         pubB = self.publickey(B)
         assert isinstance(pubB, _RSAobj)
@@ -29,20 +29,20 @@ class Original:
         rsa = PKCS1_OAEP.new(pubB, SHA256)
         assert isinstance(rsa, PKCS1OAEP_Cipher)
 
-        k = Random.get_random_bytes(n)
+        k = Random.get_random_bytes(self._n)
 
-        aesk = AES.new(k, AES.MODE_CTR, ctr)
+        aesk = AES.new(k, AES.MODE_CTR, self._ctr)
         D = aesk.encrypt(data)
 
         hmac = HMAC.new(k, D, SHA256)
         M = hmac.digest()
 
-        ABk = Container(A, B, k).serialize()
-        hashABk = SHA256.new(ABk).digest()
+        ABk_serialized = Container(A, B, k).serialize()
+        hashABk = SHA256.new(ABk_serialized).digest()
         S = signer.sign(hashABk)
 
-        ABkS = Container(A, B, k, S).serialize()
-        H = rsa.encrypt(ABkS)
+        ABkS_serialized = Container(A, B, k, S).serialize()
+        H = rsa.encrypt(ABkS_serialized)
         return Payload(H, D, M).serialize()
 
     def dec(self, payload):
@@ -53,7 +53,7 @@ class Original:
         raise NotImplementedError()
         return None
 
-    def privatekey(self, target):
+    def privatekey(self):
         raise NotImplementedError()
         return None
 
