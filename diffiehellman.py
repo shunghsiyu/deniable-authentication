7e1961bae39f686bfe801c9cb31f811fee0e1ccd
shunghsiyu <shunghsiyu@gmail.com>
2015-04-07 10:22:29 -0400

    Update diffiehellman.py
    Implement skeleton for DiffieHellman class with inheritance from
    Original class

--- diffiehellman.py
+++ diffiehellman.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from original import Original
+
 __author__ = 'shunghsiyu'
 
+
+class DiffieHellman(Original):
+    def __init__(self, identity):
+        super(DiffieHellman, self).__init__(identity)
+
+    def enc(self, data, A, B):
+        raise NotImplementedError()
+
+    def dec(self, payload_serialized):
+        raise NotImplementedError()
