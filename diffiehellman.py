4818edf78059ba456afd40f163be573181cccde5
shunghsiyu <shunghsiyu@gmail.com>
2015-04-29 12:41:53 -0400

    Update diffiehellman.py
    Complete the implementation for encryption and  decryption

--- diffiehellman.py
+++ diffiehellman.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from Finder.Containers_and_folders import container
+from gen import containerxml
 from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.PublicKey import ElGamal
+from Crypto.Util import number
 from original import Original
+import pyxb
 
 __author__ = 'shunghsiyu'
 
@@ -17,30 +21,51 @@ class DiffieHellman(Original):
     def enc(self, data, A, B):
 
         N = Random.get_random_bytes(self._n)
-        kAB = ''
-
-        ksess = HMAC.new(kAB, '0'+N, SHA256).digest()
-        ksess2 = HMAC.new(kAB, '1'+N, SHA256).digest()
+        kAB_num = pow(self.publickey(B).y, self.privatekey().x, self.privatekey().p)
+        kAB = number.long_to_bytes(kAB_num)
+        ksess = HMAC.new(kAB, '\x00'+N, SHA256).digest()
+        ksess2 = HMAC.new(kAB, '\x01'+N, SHA256).digest()
 
         aes = AES.new(ksess, AES.MODE_CTR, counter=self._ctr())
-        ABdata = (A, B, data)
-        C = aes.encrypt(ABdata)
+        ABdata_serialized = containerxml.container(pyxb.BIND(a=A, b=B, data=data)).toxml('utf-8')
+        C = aes.encrypt(ABdata_serialized)
 
-        M = HMAC.new(ksess2, C+N, SHA256).digest()
+        CN_serialized = containerxml.container(pyxb.BIND(c=C, n=N)).toxml('utf-8')
+        M = HMAC.new(ksess2, CN_serialized, SHA256).digest()
 
         payload = (C, N, M)
         return payload
 
     def dec(self, payload_serialized):
-        raise NotImplementedError()
+        C = payload_serialized[0]
+        N = payload_serialized[1]
+        M = payload_serialized[2]
+
+        # TODO: get public key
+        kAB_num = pow(self.privatekey().y, self.privatekey().x, self.privatekey().p)
+        kAB = number.long_to_bytes(kAB_num)
+        ksess = HMAC.new(kAB, '\x00'+N, SHA256).digest()
+        ksess2 = HMAC.new(kAB, '\x01'+N, SHA256).digest()
+
+        aes = AES.new(ksess, AES.MODE_CTR, counter=self._ctr())
+        ABdata = containerxml.CreateFromDocument(aes.decrypt(C))
+        A = ABdata.diffiehellman.a
+        B = ABdata.diffiehellman.b
+        data = ABdata.diffiehellman.data
+
+        CN_serialized = containerxml.container(pyxb.BIND(c=C, n=N)).toxml('utf-8')
+        M_calculated = HMAC.new(ksess2, CN_serialized, SHA256).digest()
+        assert M == M_calculated
+
+        return data
 
     def publickey(self, target):
         with open(target+'.pub', 'r') as f:
-            publickey = ElGamal.construct(f.read().splitlines())
+            publickey = ElGamal.construct(tuple(int(value) for value in f.read().splitlines()))
         return publickey
 
     def privatekey(self):
         with open(self._identity, 'r') as f:
-            privatekey = ElGamal.construct(f.read().splitlines())
+            privatekey = ElGamal.construct(tuple(int(value) for value in f.read().splitlines()))
         assert privatekey.has_private()
         return privatekey
