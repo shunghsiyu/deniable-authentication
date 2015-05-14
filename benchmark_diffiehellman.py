34efcd254ee3115dc1eb4487ad160e1112f6c6f0
shunghsiyu <shunghsiyu@gmail.com>
2015-05-13 22:52:03 -0400

    Update benchmark_diffiehellman.py
    Calculate and print size overhead
    Extract benchmark code into a single function
    Automate benchmark for different data size
    Use pre-generated ElGamal key

--- benchmark_diffiehellman.py
+++ benchmark_diffiehellman.py
@@ -1,9 +1,15 @@
+from __future__ import division
+from diffiehellman import DiffieHellman
+from Crypto import Random
 import timeit
-import sys
+import os
 __author__ = 'shunghsiyu'
 
 
-def benchmark(count, size):
+A = 'Alice'
+
+
+def benchmark(input_size, count):
     setup = """
 import random
 import string
@@ -15,29 +21,33 @@ from Crypto.PublicKey import ElGamal
 from Crypto.PublicKey.ElGamal import ElGamalobj
 from Crypto import Random
 
-A = ''.join(random.choice(string.ascii_letters) for _ in range(12))
-keyA = ElGamal.generate(2048, Random.new().read)
-with open(A, 'w') as f:
-    f.write(export_key(keyA))
-with open(A+'.pub', 'w') as f:
-    f.write(export_key(keyA.publickey()))
+A = '{}'
 obj = DiffieHellman(A)
 data = Random.get_random_bytes({})
 cipher_text = obj.enc(data, A, A)
-""".format(size)
+""".format(A, input_size)
     stmt_enc = "obj.enc(data, A, A)"
     stmt_dec = "obj.dec(cipher_text)"
     t1 = timeit.Timer(stmt=stmt_enc, setup=setup)
-    print('Test DiffieHellman ENCRYPTION with {} bytes and running {} times'.format(size, count))
+    print('Test Diffie-Hellman ENCRYPTION with {} bytes and running {} times'.format(input_size, count))
     print(t1.timeit(count) / count)
+    output_size = get_output_size(input_size)
+    print('The input size is {} and the output size is {}'.format(input_size, output_size))
+    print('The ratio is {}'.format(output_size/input_size))
     t2 = timeit.Timer(stmt=stmt_dec, setup=setup)
-    print('Test DiffieHellman DECRYPTION with {} bytes and running {} times'.format(size, count))
+    print('Test Diffie-Hellman DECRYPTION with {} bytes and running {} times'.format(input_size, count))
     print(t2.timeit(count) / count)
     print('-----')
 
+def get_output_size(input_size):
+    data = Random.get_random_bytes(input_size)
+    cipher_text = DiffieHellman(A).enc(data, A, A)
+    return len(cipher_text)
 
 if __name__ == '__main__':
-    benchmark(100*2**10, 500)
-    benchmark(1*2**20, 100)
-    benchmark(10*2**20, 10)
-    benchmark(100*2**20, 1)
+    os.chdir('keys/elgamal/')
+    run = 3
+    benchmark(100*2**10, 500*run)
+    benchmark(1*2**20, 100*run)
+    benchmark(10*2**20, 10*run)
+    benchmark(100*2**20, 1*run)
