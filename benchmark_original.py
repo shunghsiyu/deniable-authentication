863e84f8edbac0096fad45619a0a48dc4bf6b40e
shunghsiyu <shunghsiyu@gmail.com>
2015-05-14 15:13:00 -0400

    Update benchmark_original.py
    Add benchmark for methods using Base64 encoding

--- benchmark_original.py
+++ benchmark_original.py
@@ -20,23 +20,44 @@ A = '{}'
 obj = Original(A)
 data = Random.get_random_bytes({})
 cipher_text = obj.enc(data, A, A)
+cipher_text_b64 = obj.enc_base64(data, A, A)
 """.format(A, input_size)
     stmt_enc = "obj.enc(data, A, A)"
+    stmt_enc_b64 = "obj.enc_base64(data, A, A)"
     stmt_dec = "obj.dec(cipher_text)"
+    stmt_dec_b64 = "obj.dec_base64(cipher_text_b64)"
+
+    # BINARY
     t1 = timeit.Timer(stmt=stmt_enc, setup=setup)
-    print('Test Original ENCRYPTION with {} bytes and running {} times'.format(input_size, count))
+    print('Test Original ENCRYPTION to BINARY with {} bytes and running {} times'.format(input_size, count))
     print(t1.timeit(count) / count)
-    output_size = get_output_size(input_size)
-    print('The input size is {} and the output size is {}'.format(input_size, output_size))
-    print('The ratio is {}'.format(output_size/input_size))
     t2 = timeit.Timer(stmt=stmt_dec, setup=setup)
-    print('Test Original DECRYPTION with {} bytes and running {} times'.format(input_size, count))
+    print('Test Original DECRYPTION to BINARY with {} bytes and running {} times'.format(input_size, count))
     print(t2.timeit(count) / count)
+    output_size = get_output_size(input_size)
+    print('The input size is {} and the BINARY output size is {}'.format(input_size, output_size))
+    print('The ratio is {}'.format(output_size/input_size))
+    print()
+
+    # BASE64
+    t3 = timeit.Timer(stmt=stmt_enc_b64, setup=setup)
+    print('Test Original ENCRYPTION to BASE64 with {} bytes and running {} times'.format(input_size, count))
+    print(t3.timeit(count) / count)
+    t4 = timeit.Timer(stmt=stmt_dec_b64, setup=setup)
+    print('Test Original DECRYPTION to BASE64 with {} bytes and running {} times'.format(input_size, count))
+    print(t4.timeit(count) / count)
+    output_size_b64 = get_output_size(input_size, encoding='base64')
+    print('The input size is {} and the BASE64 output size is {}'.format(input_size, output_size))
+    print('The ratio is {}'.format(output_size_b64/input_size))
+
     print('-----')
 
-def get_output_size(input_size):
+def get_output_size(input_size, encoding='binary'):
     data = Random.get_random_bytes(input_size)
-    cipher_text = Original(A).enc(data, A, A)
+    if encoding.lower() in ['base64', 'b64']:
+        cipher_text = Original(A).enc_base64(data, A, A)
+    else:
+        cipher_text = Original(A).enc(data, A, A)
     return len(cipher_text)
 
 if __name__ == '__main__':
